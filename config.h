ca9ca5ded0527b5031c7123725f26738539e68c9
Jochen Stierberger <jochen@stierberger.com>
2022-12-18 13:31:43 -0600

    define combos

--- config.h
+++ config.h
@@ -1,33 +1,3 @@
 #pragma once
-
-// #define TAPPING_TERM 200
-
-// Prevent normal rollover on alphas from accidentally triggering mods.
-// #define IGNORE_MOD_TAP_INTERRUPT
-
-// // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
-// #define TAPPING_FORCE_HOLD
-
-// // Auto Shift
-// #define NO_AUTO_SHIFT_ALPHA
-// #define AUTO_SHIFT_TIMEOUT TAPPING_TERM
-// #define AUTO_SHIFT_NO_SETUP
-
-// Mouse key speed and acceleration.
-// #undef MOUSEKEY_DELAY
-// #define MOUSEKEY_DELAY          0
-// #undef MOUSEKEY_INTERVAL
-// #define MOUSEKEY_INTERVAL       16
-// #undef MOUSEKEY_WHEEL_DELAY
-// #define MOUSEKEY_WHEEL_DELAY    0
-// #undef MOUSEKEY_MAX_SPEED
-// #define MOUSEKEY_MAX_SPEED      6
-// #undef MOUSEKEY_TIME_TO_MAX
-// #define MOUSEKEY_TIME_TO_MAX    64
-
-#define COMBO_COUNT 2
-// #undef LOCKING_SUPPORT_ENABLE
-// #undef LOCKING_RESYNC_ENABLE
-// #define NO_ACTION_ONESHOT
-// #define NO_MUSIC_MODE
-// #define LAYER_STATE_8BIT
+#define DOUBLE_TAP_SHIFT_TURNS_ON_CAPS_WORD
+#define COMBO_COUNT 1
